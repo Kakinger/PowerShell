92c6c5ab5cf583dcbd917e3cffe3b83a0ab50327
Kakinger <jan@zueschen.eu>
2019-03-26 17:16:20 +0100

    Add files via upload

---
+++ Hyper-V Backup Script/Export.ps1
@@ -0,0 +1,151 @@
+﻿#region
+##############################################################################################
+# Script zum Export von VMs unter Hyper-V mit Windows Server 2016 oder Windows 10 Pro        #
+# erstellt von Jan Kappen - j.kappen@rachfahl.de                                             #
+# Version 0.4.1                                                                              #
+# 06. Mai 2017                                                                               #
+# Diese Script wird bereitgestellt wie es ist, ohne jegliche Garantie. Der Einsatz           #
+# erfolgt auf eigene Gefahr. Es wird jegliche Haftung ausgeschlossen.                        #
+# Wer dem Autor etwas Gutes tun möchte, er trinkt gern ein kaltes Corona :)                  #
+#                                                                                            #
+# Dieses Script beinhaltet keine Hilfe. RTFM.                                                #
+#                                                                                            #
+# www.hyper-v-server.de | www.rachfahl.de                                                    #
+#                                                                                            #
+##############################################################################################
+#endregion
+
+Param(
+	[string] $VM ="",
+	[string] $Exportpfad = "",
+	[string] $Logpfad = "",
+	[switch] $Speichern,
+    [switch] $ProductionCheckpoint,
+    [switch] $Herunterfahren,
+    [switch] $statusmail
+)
+
+function Mail
+{
+    Write-Host -ForegroundColor Red (Get-Date) "Die Mail-Konfiguration muss angepasst werden, sonst ist kein Mailversand möglich!"
+    # $mail = @{
+    #    SmtpServer = 'mailer.domain.loc'
+    #    Port = 25
+    #    From = 'backupskript@rachfahl.de'
+    #    To = 'empfaenger@domain.loc'
+    #    Subject = "'Script-Sicherung der VM' $VM"
+    #    Body = "'Anbei das Log der Export-Sicherung von VM' $VM"
+    #    Attachments = "$Logdatei"
+    # }
+    # Send-MailMessage @mail
+}
+
+#region Logdatei
+$LogDateiDatum = Get-Date -Format yyyy-MM-dd
+if (!$Logpfad) {
+                    $LogPfadVorhanden = Test-Path ${env:homedrive}\windows\Logs\HyperVExport\
+                    if ($LogPfadVorhanden -eq $False) { new-item ${env:homedrive}\windows\Logs\HyperVExport\ -itemtype directory }
+                    $Logdatei = "${env:homedrive}\windows\Logs\HyperVExport\$LogDateiDatum.log" 
+                }
+                else 
+                { $Logdatei = "$Logpfad\$LogDateiDatum.log" }
+# Start logging
+Start-Transcript -Path $Logdatei -Append
+#endregion
+
+#region Abfrage auf benötigte Parameter
+if (!$VM) { 
+              Write-Host -ForegroundColor Red (Get-Date) "Parameter -VM muss vorhanden sein und einen Namen enthalten. Abbruch!"
+              exit 
+          }
+if (!$Exportpfad) { 
+                      Write-Host -ForegroundColor Red (Get-Date)  "Parameter -RemotePfad muss vorhanden sein und einen Pfad enthalten. Abbruch!"
+                      exit
+                  }
+#endregion
+
+#region Export der VM
+if ($Speichern -match "true")
+{
+    Write-Host -ForegroundColor Green (Get-Date) "VM wird gespeichert"
+    Save-VM -Name $VM -verbose
+    Write-Host -ForegroundColor Green (Get-Date) "Export der VM"
+    $ZielPfadVorhanden = Test-Path $Exportpfad\$VM
+    if 
+        ($ZielPfadVorhanden -eq $False) { Export-VM -Name $VM -Path $Exportpfad -verbose }
+    else 
+        { Remove-Item -Recurse -Force $Exportpfad\$VM -verbose; Export-VM -Name $VM -Path $Exportpfad -verbose }
+    Write-Host -ForegroundColor Green (Get-Date) "Export abgeschlossen, VM wird wieder eingeschaltet"
+    Start-VM -Name $VM -verbose
+    if ($statusmail -match "true")
+        { Mail; exit }
+    else
+        { exit }
+}
+
+if ($Herunterfahren -match "true")
+{
+    $ShutdownStatus = get-vm -Name $VM | Get-VMIntegrationService | where { $_.Name -EQ "Shutdown" -or $_.Name -EQ "Herunterfahren" }
+    if ($ShutdownStatus.Enabled -eq "True")
+    {
+        Write-Host -ForegroundColor Green (Get-Date) "Export scheint möglich zu sein, VM wird heruntergefahren"
+        Stop-VM -Name $VM -Force -verbose
+        Write-Host -ForegroundColor Green (Get-Date) "Export der VM"
+        $ZielPfadVorhanden = Test-Path $Exportpfad\$VM
+        if 
+            ($ZielPfadVorhanden -eq $False) { Export-VM -Name $VM -Path $Exportpfad -verbose }
+        else 
+            { Remove-Item -Recurse -Force $Exportpfad\$VM -verbose; Export-VM -Name $VM -Path $Exportpfad -verbose }
+        Write-Host -ForegroundColor Green (Get-Date) "VM wird wieder eingeschaltet"
+        Start-VM -Name $VM -verbose
+        if ($statusmail -match "true")
+            { Mail; exit }
+        else
+            { exit }
+    }
+    else
+    {
+        Write-Host -ForegroundColor Red (Get-Date) "Export scheint nicht möglich zu sein, Vorgang wird abgebrochen!"
+        exit
+    }
+}
+
+if ($ProductionCheckpoint -match "true")
+{
+    $SnapshotName = "ExportScriptCheckpoint"
+    Write-Host -ForegroundColor Green (Get-Date) "Checkpoint wird erstellt"
+    $ZielPfadVorhanden = Test-Path $Exportpfad\$VM
+    if 
+        ($ZielPfadVorhanden -eq $False) 
+        { Checkpoint-VM -Name $VM -SnapshotName $SnapshotName -verbose
+          Export-VMSnapshot -VMName $VM -Name $SnapshotName -Path $Exportpfad -verbose
+          Remove-VMSnapshot -VMName $VM -Name $SnapshotName -verbose
+        }
+    else 
+        { Remove-Item -Recurse -Force $Exportpfad\$VM -verbose
+          Checkpoint-VM -Name $VM -SnapshotName $SnapshotName -verbose
+          Export-VMSnapshot -VMName $VM -Name $SnapshotName -Path $Exportpfad -verbose
+          Remove-VMSnapshot -VMName $VM -Name $SnapshotName -verbose
+        }
+    Write-Host -ForegroundColor Green (Get-Date) "Export abgeschlossen"
+    if ($statusmail -match "true")
+        { Mail; exit }
+    else
+        { exit }
+}
+else
+{
+    Write-Host -ForegroundColor Green (Get-Date) "VM wird online exportiert"
+    Write-Host -ForegroundColor Green (Get-Date) "Export der VM"
+    $ZielPfadVorhanden = Test-Path $Exportpfad\$VM -verbose
+    if 
+        ($ZielPfadVorhanden -eq $False) { Export-VM -Name $VM -Path $Exportpfad -verbose }
+    else 
+        { Remove-Item -Recurse -Force $Exportpfad\$VM -verbose; Export-VM -Name $VM -Path $Exportpfad -verbose }
+    Write-Host -ForegroundColor Green (Get-Date) "Export der VM $VM abgeschlossen"
+    if ($statusmail -match "true")
+        { Mail; exit }
+    else
+        { exit }
+}
+#endregion
---
+++ Hyper-V Backup Script/zusätzliche Befehle und Funktionen.ps1
@@ -0,0 +1,167 @@
+﻿#region Test auf Integrationskomponente "Herunterfahren"
+function ComponentFunction
+{
+    $vmShutdownIC = get-vm -Name $VM | Get-VMIntegrationService
+    if ($vmShutdownIC.name -eq "Shutdown")
+    {
+        $lang = "ENG"
+        write-host $lang
+    }
+    else
+    {
+        if ($vmShutdownIC.name -eq "Herunterfahren")
+        {
+            $lang = "DEU"
+            write-host $lang
+        }
+        else
+        {
+            write-host -ForegroundColor Red "Abbruch"
+        }
+    }
+}
+#endregion
+############################################################
+#region Überprüfung auf shutdown - ALT!
+$vmShutdownIC = Get-VM –Name $VM | Get-VMIntegrationService –Name Herunterfahren
+if($vmShutdownIC.enabled -match "True")
+    {
+    if ($verbose -eq $true) { Write-Host (Get-Date) "Der Integrationsdienst 'Herunterfahren' ist aktiviert" }
+                              $temp1 = (Get-Date)
+                              $temp2 = "Der Integrationsdienst 'Herunterfahren' ist aktiviert"
+                              "$temp1 - $temp2" | Out-File $Logdatei -Append
+    }
+    else
+    {
+        if ($verbose -eq $true) { Write-Host (Get-Date) "Der Integrationsdienst 'Herunterfahren' ist NICHT aktiviert. VM kann nur gespeichert werden!" }
+                                  $temp1 = (Get-Date)
+                                  $temp2 = "Der Integrationsdienst 'Herunterfahren' ist NICHT aktiviert. VM kann nur gespeichert werden!"
+                                  "$temp1 - $temp2" | Out-File $Logdatei -Append
+            if($Speichern.IsPresent -match "True") { 
+                                                     if ($verbose -eq $true) { Write-Host (Get-Date) "VM kann trotzdem exportiert werden, da sie gespeichert wird" }
+                                                     $temp1 = (Get-Date)
+                                                     $temp2 = "VM kann trotzdem exportiert werden, da sie gespeichert wird"
+                                                     "$temp1 - $temp2" | Out-File $Logdatei -Append
+                                                   }
+            else
+            { 
+            if ($verbose -eq $true) { Write-Host (Get-Date) "Vm wird nicht exportiert, Abbruch!" }
+            $temp1 = (Get-Date)
+            $temp2 = "Vm wird nicht exportiert, Abbruch!"
+            $temp3 = "---------------------------------"
+            "$temp1 - $temp2" | Out-File $Logdatei -Append
+            "$temp3" | Out-File $Logdatei -Append
+            exit 
+            }
+    }
+#endregion
+
+#region Status der VM
+$vmstatus = Get-VM –Name $VM
+    if($vmstatus.State -match "Running")
+    {
+        if ($verbose -eq $true) { Write-Host (Get-Date) "VM ist eingeschaltet" }
+                                $temp1 = (Get-Date)
+                                $temp2 = "VM ist eingeschaltet"
+                                "$temp1 - $temp2" | Out-File $Logdatei -Append
+        #################################
+        # Abfrage auf Speichern-Zustand #
+    	#################################
+        if($Speichern.IsPresent -match "True")
+            {
+        ####################
+        # Speichern der VM #
+    	####################
+                if ($verbose -eq $true) { Write-Host (Get-Date) "VM wird gespeichert" }
+                                        $temp1 = (Get-Date)
+                                        $temp2 = "VM wird gespeichert"
+                                        "$temp1 - $temp2" | Out-File $Logdatei -Append
+            Save-VM -Name $VM
+               }
+            else
+            {
+        #########################################
+        # Kein Speichern, Herunterfahren der VM #
+    	#########################################
+                if ($verbose -eq $true) { Write-Host (Get-Date) "VM wird heruntergefahren" }
+                                        $temp1 = (Get-Date)
+                                        $temp2 = "VM wird heruntergefahren"
+                                        "$temp1 - $temp2" | Out-File $Logdatei -Append
+            ################################
+            # Warten auf ausgeschaltete VM #
+    	    ################################
+            Stop-VM -Name $VM -Force
+            }
+        }
+    else
+    {
+        if ($verbose -eq $true) { Write-Host (Get-Date) "VM ist bereits ausgeschaltet" }
+                                $temp1 = (Get-Date)
+                                $temp2 = "VM ist bereits ausgeschaltet"
+                                "$temp1 - $temp2" | Out-File $Logdatei -Append
+    }
+#endregion
+########################################
+#region Start der VM
+if ($verbose -eq $true) { Write-Host (Get-Date) "Ueberpruefung auf Startverhalten nach Export" }
+                        $temp1 = (Get-Date)
+                        $temp2 = "Ueberpruefung auf Startverhalten nach Export"
+                        "$temp1 - $temp2" | Out-File $Logdatei -Append
+        ###################################
+        # Ueberpruefung auf Stop-Schalter #
+        ###################################
+        if ($Auslassen.IsPresent -eq $False)
+            { if ($verbose -eq $true) { Write-Host (Get-Date) "VM wird eingeschaltet" }
+                                    $temp1 = (Get-Date); $temp2 = "VM wird eingeschaltet"; "$temp1 - $temp2" | Out-File $Logdatei -Append
+            Start-VM -Name $VM }
+        else
+            { if ($verbose -eq $true) { Write-Host (Get-Date) "VM bleibt ausgeschaltet" }
+                                    $temp1 = (Get-Date)
+                                    $temp2 = "VM bleibt ausgeschaltet"
+                                    $temp3 = "---------------------------------"
+                                    "$temp1 - $temp2" | Out-File $Logdatei -Append
+                                    "$temp3" | Out-File $Logdatei -Append }
+#endregion
+#################################################
+function MailFunction
+{
+    if ($verbose -eq $true) { Write-Host (Get-Date) "Ueberpruefung auf Sendung einer Status-Mail" }
+                        $temp1 = (Get-Date)
+                        $temp2 = "Ueberpruefung auf Sendung einer Status-Mail"
+                        "$temp1 - $temp2" | Out-File $Logdatei -Append
+###########################################################################
+
+        if ($statusmail.IsPresent -eq $False)
+            { if ($verbose -eq $true) { Write-Host (Get-Date) "Es wird keine Email versendet" }
+                                    $temp1 = (Get-Date)
+                                    $temp2 = "Es wird keine Email versendet"
+                                    "$temp1 - $temp2" | Out-File $Logdatei -Append }
+        else
+##################################
+# Email versenden mit Status-Log #
+##################################
+         { if ($verbose -eq $true) { Write-Host (Get-Date) "Es wird eine Email versendet" }
+                                   $temp1 = (Get-Date)
+                                   $temp2 = "Es wird eine Email versendet"
+                                   "$temp1 - $temp2" | Out-File $Logdatei -Append
+
+########################################################################
+# Ab hier muessen die persoenlichen Einstellungen konfiguriert werden! #
+########################################################################
+
+{ if ($verbose -eq $true) { Write-Host (Get-Date) "Diese Zeile muss auskommentiert werden!" }
+
+# $mail = @{
+#    SmtpServer = 'mailer.domain.loc'
+#    Port = 25
+#    From = 'backupskript@rachfahl.de'
+#    To = 'empfaenger@domain.loc'
+#    Subject = "'Script-Sicherung der VM' $VM"
+#    Body = "'Anbei das Log der Export-Sicherung von VM' $VM"
+#    Attachments = "$Logdatei"
+# }
+# Send-MailMessage @mail }
+} }
+}
+#endregion
+
