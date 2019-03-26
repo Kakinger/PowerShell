9b36eca00aa6e0dc4bfa7a181bf48fce568dc280
Kakinger <jan@zueschen.eu>
2019-03-26 17:40:25 +0100

    Add files via upload

---
+++ Firefox Silent Installation/Installation_Foxit_Silent_Powershell.ps1
@@ -0,0 +1,7 @@
+﻿$servers = "Hyperv10", "Hyperv11", "Hyperv12", "Hyperv13", "Hyperv14", "Hyperv15", "Hyperv16", "Hyperv17", "Hyperv18", "Hyperv19", "Hyperv20", "Hyperv21"
+foreach ($server in $servers)
+{
+    New-item -ItemType Directory -Path "\\$server\c$\system"
+    Copy-Item -Path "C:\system\FoxitReader83_L10N_Setup_Prom.exe" -Destination "\\$server\c$\system\"
+    Invoke-Command -ComputerName $server -ScriptBlock { Start-Process -FilePath "C:\system\FoxitReader83_L10N_Setup_Prom.exe" -ArgumentList '/ForceInstall /VERYSILENT DESKTOP_SHORTCUT="1" MAKEDEFAULT="1" VIEWINBROWSER="0" LAUNCHCHECKDEFAULT="1" AUTO_UPDATE="2" /passive /norestart' -Wait }
+}
